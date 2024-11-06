5912163a08988eaeda83fc474e8e3bb29442d8bf
anamontero14 <ana.montero@iesnervion.es>
2024-11-06 08:52:55 +0100

    Sincronización

---
+++ Tema3/src/ejerciciosparte1/Ejer1.java
@@ -0,0 +1,11 @@
+package ejerciciosparte1;
+
+public class Ejer1 {
+
+	public static void main(String[] args) {
+		
+		/**/
+
+	}
+
+}
---
+++ Tema3/src/pruebas/Pr1.java
@@ -0,0 +1,10 @@
+package pruebas;
+
+public class Pr1 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
