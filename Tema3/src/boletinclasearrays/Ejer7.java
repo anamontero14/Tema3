e9464800877e8bf2bd98017b0413d8ef47646ae7
anamontero14 <ana.montero@iesnervion.es>
2024-11-11 18:30:09 +0100

    Ejercicio 7 terminado

--- Tema3/src/boletinclasearrays/Ejer7.java
+++ Tema3/src/boletinclasearrays/Ejer7.java
@@ -1,9 +1,44 @@
 package boletinclasearrays;
 
+import java.util.Arrays;
+
 public class Ejer7 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		/*
+		 * Crea un programa que cree un array de tipo entero e introduzca la siguiente
+		 * secuencia de valores: 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, etc. hasta
+		 * introducir 10 diez veces el 10, y luego la muestre por pantalla. En esta
+		 * ocasión has de utilizar Arrays.fill().
+		 */
+
+		// creación de la tabla
+		int tabla[] = new int[55];
+
+		// inicio
+		int inicio = 0;
+
+		// hasta
+		int hasta = 1;
+
+		// numero
+		int num = 1;
+
+		for (int i = 0; i < 10; i++) {
+
+			for (int j = 0; j <= i; j++) {
+
+				Arrays.fill(tabla, inicio, hasta, num);
+
+				System.out.print(tabla[i] + ", ");
+
+				inicio++;
+				hasta++;
+				num++;
+
+			}
+		}
 
 	}
 
