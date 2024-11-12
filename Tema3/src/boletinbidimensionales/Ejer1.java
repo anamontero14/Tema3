9b7409a35d35de8a215d662ce120c04738b18576
anamontero14 <ana.montero@iesnervion.es>
2024-11-12 14:36:06 +0100

    Ejercicio 1 BIDIMENSIONALES

---
+++ Tema3/src/boletinbidimensionales/Ejer1.java
@@ -0,0 +1,17 @@
+package boletinbidimensionales;
+
+public class Ejer1 {
+
+	public static void main(String[] args) {
+		/*
+		 * Define un array de números enteros de 3 filas por 6 columnas con nombre num y
+		 * asigna los valores según la siguiente tabla. Muestra el contenido de todos
+		 * los elementos del array dispuestos en forma de tabla como se muestra en la
+		 * figura
+		 */
+
+		// creacion del array
+
+	}
+
+}
