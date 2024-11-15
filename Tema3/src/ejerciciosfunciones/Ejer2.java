99fe85b5d7d486ddff913594df9eac5e6c85db36
anamontero14 <ana.montero@iesnervion.es>
2024-11-15 14:36:49 +0100

    Ejercicio 2 empezado

---
+++ Tema3/src/ejerciciosfunciones/Ejer2.java
@@ -0,0 +1,13 @@
+package ejerciciosfunciones;
+
+public class Ejer2 {
+
+	public static void main(String[] args) {
+		/*
+		 * Diseñar la función: int maximo(int t[]), que devuelva el máximo valor
+		 * contenido en la tabla t.
+		 */
+
+	}
+
+}
